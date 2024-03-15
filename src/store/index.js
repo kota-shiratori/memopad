c9b80a306cec93502bc61b0956e5fe5c14718585
kota_shiratori <swan.kouta@gmail.com>
2024-03-15 22:20:39 +0900

    🎨 フォームとVuexを連携

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count: 0,
+    memos: []
   },
   getters: {
   },
   mutations: {
+    /* メモを保存する */
+    save(state,newMemo) {
+      newMemo.id = ++state.count
+      state.memos.unshift(newMemo)
+    }
+
   },
   actions: {
   },
